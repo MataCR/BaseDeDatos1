dcf79b066869740a0b07170afebcf9e0147517d6
luisg <luisg@DESKTOP-CE2ABDN>
2019-11-03 16:30:14 -0600

    Merge origin/master
    Conflicts:
    	src/bases/interfaces/registro_profesor.java

--- src/bases/interfaces/consultaReporte.java
+++ src/bases/interfaces/consultaReporte.java
@@ -78,6 +78,11 @@ public class consultaReporte extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTable1);
 
         jButton1.setText("Consultar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -120,6 +125,11 @@ public class consultaReporte extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
